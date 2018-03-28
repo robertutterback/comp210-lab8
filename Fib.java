67771bdcd3b78f159155f0a60dae9fd9e5189027
Lukas <lukas@lukas-mbp.local>
2018-03-28 14:45:41 -0500

    edited the fib code

--- Fib.java
+++ Fib.java
@@ -1,15 +1,12 @@
 public class Fib {
   // compute the nth fibonacci number
-  public static int fib(int n) {
-    static int fib(int n){
-      if (n <= 1){
-        return n;
-      }
-    return fib(n-1) + fib(n-2);
+    public static int fib(int n){ 
+	if (n <= 1){
+	    return n;
+	}
+	return fib(n-1) + fib(n-2);
     }
-  }
-
-  public static void main(String[] args) {
-    System.out.println(fib(10));
-  }
+    public static void main(String[] args) {
+	System.out.println(fib(10));
+    }
 }

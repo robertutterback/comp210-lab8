d0487ffd48b261441b6df63545459ac8305d94cf
Robert Utterback <robertu13@gmail.com>
2018-03-28 13:43:30 -0500

    Basic skeleton with fib function stub.

---
+++ Fib.java
@@ -0,0 +1,11 @@
+public class Fib {
+
+  // compute the nth fibonacci number
+  public static int fib(int n) {
+    
+  }
+
+  public static void main(String[] args) {
+    System.out.println(fib(10));
+  }
+}

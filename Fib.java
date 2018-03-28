1ed7ad050b654de2b37b0bf710b3d97ec0e72b8c
Awallace8074 <Awallace@monmouthcollege.edu>
2018-03-28 14:59:10 -0500

    this is the fibonacci number

--- Fib.java
+++ Fib.java
@@ -2,7 +2,7 @@ public class Fib {
 
   // compute the nth fibonacci number
   public static int fib(int n) {
-    
+      return fib(n-1)+fib(n-2);
   }
 
   public static void main(String[] args) {

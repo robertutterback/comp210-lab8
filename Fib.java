81912ed616e0946eb09eea0e322a25eb04d9952e
nsmolczyk <nsmolczyk@gmail.com>
2018-03-28 14:38:53 -0500

    added fibonacci algorithm

--- Fib.java
+++ Fib.java
@@ -2,6 +2,14 @@ public class Fib {
 
   // compute the nth fibonacci number
   public static int fib(int n) {
+
+      if(n==0){
+	  return 0;
+      }else if(n==1){
+	  return 1;
+      }
+
+      return fib(n-1) + fib(n-2);
     
   }
 
